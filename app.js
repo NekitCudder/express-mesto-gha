18900145807674d20ba55ca7fce2acecb2e5f838
Nikita Boev <nekitcudder@yandex.ru>
2022-10-24 14:59:35 +0300

    feat: создать схемы и модели

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb');
