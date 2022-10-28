fa64d31afb81c1bb00f2e8c215ecc8b5bedb709b
Nikita Boev <nekitcudder@yandex.ru>
2022-10-28 13:30:34 +0300

    hotfix: исправить мелкие недочеты

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,10 @@
 const userRouter = require('express').Router();
 const {
-  getUsers, getUsersById, createUser, updateUser, updateAvatar,
+  getUsers, getUserById, createUser, updateUser, updateAvatar,
 } = require('../controllers/users');
 
 userRouter.get('/users', getUsers);
-userRouter.get('/users/:userId', getUsersById);
+userRouter.get('/users/:userId', getUserById);
 userRouter.post('/users', createUser);
 userRouter.patch('/users/me', updateUser);
 userRouter.patch('/users/me/avatar', updateAvatar);
