89efca758ab1181d956e5da113d2667e75e4856e
Nikita Boev <nekitcudder@yandex.ru>
2022-10-28 12:02:32 +0300

    feat: добавить обработку ошибок

--- controllers/cards.js
+++ controllers/cards.js
@@ -3,38 +3,68 @@ const Card = require('../models/card');
 module.exports.getCards = (req, res) => {
   Card.find({})
     .then((cards) => res.send({ data: cards }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(404).send({ message: 'Запрашиваемая карточка не найдена.' });
+      } else {
+        res.status(500).send({ message: 'Ошибка' });
+      }
+    });
 };
 
 module.exports.createCards = (req, res) => {
   const { name, link } = req.body;
   Card.create({ name, link })
     .then((cards) => res.send({ data: cards }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Переданы некорректные данные.' });
+      } else {
+        res.status(500).send({ message: 'Ошибка' });
+      }
+    });
 };
 
 module.exports.deleteCards = (req, res) => {
   Card.findByIdAndRemove(req.params.id)
     .then((cards) => res.send({ data: cards }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Ошибка удаления карточки.' });
+      } else {
+        res.status(500).send({ message: 'Ошибка' });
+      }
+    });
 };
 
 module.exports.likeCard = (req, res) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
-    { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
+    { $addToSet: { likes: req.user._id } },
     { new: true },
   )
     .then((cards) => res.send({ data: cards }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Ошибка лайка карточки.' });
+      } else {
+        res.status(500).send({ message: 'Ошибка' });
+      }
+    });
 };
 
 module.exports.dislikeCard = (req, res) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
-    { $pull: { likes: req.user._id } }, // убрать _id из массива
+    { $pull: { likes: req.user._id } },
     { new: true },
   )
     .then((cards) => res.send({ data: cards }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Ошибка дизлайка карточки.' });
+      } else {
+        res.status(500).send({ message: 'Ошибка' });
+      }
+    });
 };
--- controllers/users.js
+++ controllers/users.js
@@ -3,32 +3,62 @@ const User = require('../models/user');
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then((users) => res.send({ data: users }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(404).send({ message: 'Запрашиваемый пользователь не найден.' });
+      } else {
+        res.status(500).send({ message: 'Ошибка' });
+      }
+    });
 };
 
 module.exports.getUsersById = (req, res) => {
-  User.findById(req.params.id)
+  User.findById(req.params.userId)
     .then((users) => res.send({ data: users }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(404).send({ message: 'Запрашиваемый пользователь не найден.' });
+      } else {
+        res.status(500).send({ message: 'Ошибка' });
+      }
+    });
 };
 
 module.exports.createUsers = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then((users) => res.send({ data: users }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Переданы некорректные данные.' });
+      } else {
+        res.status(500).send({ message: 'Ошибка' });
+      }
+    });
 };
 
 module.exports.updateUsers = (req, res) => {
   const { name, about } = req.body;
   User.findByIdAndUpdate(req.user._id, { name, about })
     .then((users) => res.send({ data: users }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Переданы некорректные данные.' });
+      } else {
+        res.status(500).send({ message: 'Ошибка' });
+      }
+    });
 };
 
 module.exports.updateAvatar = (req, res) => {
   const { avatar } = req.body;
   User.findByIdAndUpdate(req.user._id, { avatar })
     .then((users) => res.send({ data: users }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Переданы некорректные данные.' });
+      } else {
+        res.status(500).send({ message: 'Ошибка' });
+      }
+    });
 };
