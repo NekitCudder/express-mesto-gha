e56ca0cf9bf7bd92472d2fd62da04bdd9592c3a7
Nikita Boev <nekitcudder@yandex.ru>
2022-11-13 13:54:46 +0300

    test:

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,11 +2,12 @@ const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    next(new UnauthorizedError('Ошибка авторизации.'));
+  const token = req.cookies.jwt;
+
+  if (!token) {
+    throw new UnauthorizedError('Ошибка авторизации.');
   }
-  const token = authorization.replace('Bearer ', '');
+
   let payload;
   try {
     payload = jwt.verify(token, 'some-secret-key');
