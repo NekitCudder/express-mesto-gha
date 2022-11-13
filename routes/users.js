88dcd428840f2de94647a4c3ce7f81828312047a
Nikita Boev <nekitcudder@yandex.ru>
2022-11-13 20:26:59 +0300

    test:

--- routes/users.js
+++ routes/users.js
@@ -4,9 +4,9 @@ const {
 } = require('../controllers/users');
 const { idValidation, userInfoValidation, avatarValidation } = require('../middlewares/validations');
 
+userRouter.get('/me', currentUser);
 userRouter.get('/', getUsers);
 userRouter.get('/:_id', idValidation, getUserById);
-userRouter.get('/me', currentUser);
 userRouter.patch('/me', userInfoValidation, updateUser);
 userRouter.patch('/me/avatar', avatarValidation, updateAvatar);
 
