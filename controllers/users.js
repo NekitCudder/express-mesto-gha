0ecdb7d871ce4b8fcb052a29d0accaa0bff9e68b
Nikita Boev <nekitcudder@yandex.ru>
2022-11-13 19:35:12 +0300

    test:

--- controllers/users.js
+++ controllers/users.js
@@ -95,9 +95,8 @@ module.exports.login = (req, res, next) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
-      // res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true })
-      //   .send({ message: 'Авторизация успешна' });
-      res.send({ token });
+      res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true })
+        .send({ message: 'Авторизация успешна' });
     })
     .catch((err) => {
       if (err.name === 'ValidationError') {
